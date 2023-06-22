e8ea4feb1dc363b5d156576ad3eef3e108bb13af
RatCheese1608 <raymichaellutan@gmail.com>
2023-06-23 01:32:42 +0700

    regular push updates

--- script.js
+++ script.js
@@ -1,13 +1,14 @@
 var serv={}, cImage, promised=0;
 var qPic;
 var infbox;
+var cheese;
 
 const stor = "storageIndex.json";
 const rand = "random.json"
 
 var lock = true;
 var ansOpt="";
-var attcc=0;
+var turn=1;
 var enemy={hp:100}, player={hp:100};
 
 function getData(api, callback) {
@@ -21,6 +22,24 @@ function getData(api, callback) {
 			return dat;
 		})
 		.catch(err => console.log(err))
+}
+
+function getOffTop(elm) {
+	let total = 0;
+	while (elm) {
+		total += elm.offsetTop;
+		elm = elm.offsetParent;
+	}
+	return total;
+}
+
+function getOffLeft(elm) {
+	let total = 0;
+	while (elm) {
+		total += elm.offsetLeft;
+		elm = elm.offsetParent;
+	}
+	return total;
 }
 
 function setAnsOpt(n) {
@@ -41,8 +60,12 @@ function updtinfo(n) {
 	},150);
 }
 
+function q_event() {
+	console.log('Question TIME!')
+}
+
 function upd_hel(item, dec) {
-	item['hp']-=dec;
+	item['hp']=Math.max(item['hp']-dec, 0);
 	item['elm'].style.width=item['hp']+'%';
 }
 
@@ -52,13 +75,44 @@ function en_att() {
 	setTimeout(()=>lock=false, 200);
 }
 
+function cheesefly(source, target) {
+	let sourceTop = getOffTop(source);
+	let sourceLeft = getOffLeft(source);
+	let targetTop = getOffTop(target);
+	let targetLeft = getOffLeft(target);
+	let chez = {
+		'width':cheese.querySelector('img').width,
+		'height':cheese.querySelector('img').height
+	}
+	cheese.style.top=sourceTop+(source.height-chez.height)/2+'px';
+	cheese.style.left=sourceLeft+(source.width-chez.width)/2+'px';
+	setTimeout(()=>{
+		cheese.style.transitionDuration='.4s';
+		cheese.style.opacity="100";
+		cheese.style.top=targetTop+(target.height-chez.height)/2+'px';
+		cheese.style.left=targetLeft+(target.width-chez.width)/2+'px';
+		setTimeout(()=>{
+			cheese.style.transitionDuration='.1s';
+			cheese.style.opacity="0";
+		},500)
+	},100);
+}
+
 // attack...
 function attack() {
+	if (!turn%3) {
+		q_event()
+		return;
+	}
 	if (lock) return
 	lock = true;
-	upd_hel(enemy,10);
-	updtinfo("Sang Regular Rat menyerang Sang Golden Rat!");
+	cheesefly(player['img'],enemy['img'])
+	setTimeout(()=>{
+		upd_hel(enemy,10);
+		updtinfo("Sang Regular Rat menyerang Sang Golden Rat!");
+	},600);
 	setTimeout(en_att,1000);
+	turn++;
 }
 
 // defend...
@@ -92,9 +146,12 @@ function main() {
 window.onload = ()=> {
 	qPic = document.querySelector('img[alt="question pic"]')
 	enemy['elm'] = document.querySelector("#enemy-container .hpdisplay");
+	enemy['img'] = document.querySelector("#enemy-container img");
 	player['elm'] = document.querySelector("#player-container .hpdisplay");
+	player['img'] = document.querySelector("#player-container .sprite img");
 	infbox = document.querySelector("#actinfo p")
+	cheese = document.querySelector("#cheese-ball")
 	getData('storageIndex.json', main)
-	getData('random.json', main)
+	// getData('random.json', main)
 	lock=false;
 }
